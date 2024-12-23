e1876f54b81ea61c595c55cd91e2e17d584be278
Daniel Cavazos <daniel@caaz.me>
2024-12-22 19:33:06 -0800

    CSS only dropdowns

---
+++ themes/caaz/assets/js/main.js
@@ -0,0 +1,14 @@
+// docReady(() => {
+//   document.getElementById("page-menu").addEventListener("click", (e) => {
+//     for (const child of e.target.children) {
+//       console.log(child.attributes)
+//       if (child.tagName === "INPUT" && child.getAttribute("type") === "checkbox") {
+//         console.log("checkbox found");
+//         var current_checkedness = child.checked 
+//         console.log(current_checkedness)
+//         child.checked = !child.checked
+//       }
+//     }
+//   });
+  
+// })
